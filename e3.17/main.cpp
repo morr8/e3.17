67324197ec5ff8578b5f4aaf06ecb06395f78ac3
Sergio Campos <morrodogg8@gmail.com>
2019-07-15 15:28:45 -0700

    finished

--- e3.17/main.cpp
+++ e3.17/main.cpp
@@ -1,15 +1,30 @@
-//
-//  main.cpp
-//  e3.17
-//
-//  Created by Sergio campos on 7/15/19.
-//  Copyright © 2019 Sergio campos. All rights reserved.
-//
+/*
+ • E3.17
+ Write a program that prompts the user to provide a single character from the alphabet. Print Vowel or Consonant, depending on the user input. If the user input is not a letter (between a and z or A and Z), or is a string of length > 1, print an error message.
+ */
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main()
+{
+    cout << "enter a single character: ";
+    string letter;
+    cin >> letter;
+    
+    if (letter.length() > 1)
+    {
+        cout << "*error* just one character";
+    }
+    else if (letter == "a" || letter == "A" || letter == "e" || letter == "E" || letter == "i" || letter == "I" || letter == "o" || letter == "O" || letter == "u" || letter == "U")
+    {
+        cout << "Vowel";
+    }
+    else
+    {
+        cout << "Consonant";
+    }
+    cout << endl;
 }
